b72397f3643ee02436081e6598e68aca2e76dbf1
Aaron Bradley <abeats20@gmail.com>
2015-10-28 15:28:43 -0500

    initial commit

--- RetroSkatr/GameScene.swift
+++ RetroSkatr/GameScene.swift
@@ -10,36 +10,21 @@ import SpriteKit
 
 class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
-        
-        self.addChild(myLabel)
+        setupBackground()
     }
-    
-    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        /* Called when a touch begins */
-        
-        for touch in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
-    }
+
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
     }
+  
+  func setupBackground() {
+    let bg = SKSpriteNode(imageNamed: "bg1")
+    bg.position = CGPointMake(517, 400)
+    addChild(bg)
+    
+    let bg2 = SKSpriteNode(imageNamed: "bg2")
+    bg2.position = CGPointMake(517, 450)
+    
+  }
 }
